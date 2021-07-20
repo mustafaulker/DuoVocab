d4be997cca460563ade9e950e76eec6065aec76f
mustafaulker <mcubjk@gmail.com>
2021-07-20 21:45:30 +0300

    Initial commit

---
+++ DuoVocab/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DuoVocab project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DuoVocab.settings')
+
+application = get_wsgi_application()
