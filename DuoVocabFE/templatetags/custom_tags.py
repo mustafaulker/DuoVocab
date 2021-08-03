10dba90e8672dd19543db88a858bb05b3dfcc2d7
mustafaulker <mcubjk@gmail.com>
2021-08-03 17:40:36 +0300

    Redesign known words table

--- DuoVocabFE/templatetags/custom_tags.py
+++ DuoVocabFE/templatetags/custom_tags.py
@@ -5,9 +5,10 @@ register = template.Library()
 
 @register.filter
 def get_words(dictio, lang):
-    return dictio.get(lang, '-')
+    return dictio.get(lang, ['Seems like there is no any Word yet.'])
 
 
 @register.filter
 def get_translation(dictio, args):
-    return dictio.get(args[0]).get(args[1]) if dictio.get(args[0], {}).get(args[1], '-') else ['No Translation']
+    return dictio.get(args[0]).get(args[1]) if dictio.get(args[0], {})\
+        .get(args[1], ['Seems like there is no any Word yet.']) else ['No Translation']
